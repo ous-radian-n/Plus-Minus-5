d5a62e20c69870b6d780f6fe11bbc4d9fcabf02a
LAPTOP-V2U6OTPF\keita <keita13243546576879@gmail.com>
2022-01-29 17:52:37 +0900

    Implemented keyconfig.

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -26,6 +26,15 @@ public class GameDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Keypad4)|| Input.GetKeyDown(KeyCode.Alpha4)|| Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
 
     }
         }
