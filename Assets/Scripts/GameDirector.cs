368341c27f49854a982a83f2a40650eb5a96a350
ous-radian-n <snine9801@gmail.com>
2022-01-29 18:29:07 +0900

    GameDirector周りの実装（α版開発）

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 
 public class GameDirector : MonoBehaviour
 {
+    public Image sliderNextNumImage;
+    [SerializeField]
+    List<Sprite> numImage = new List<Sprite>();
+
     public Text textUI;
 
+    int nowNum, nextNum, leftNum = 0, rightNum = 0;
+    [SerializeField]
+    Image nowNumImage, nextNumImage, leftNumImage, rightNumImage;
 
     // Start is called before the first frame update
     void Start()
     {
-       // float speed = 0;
+        // float speed = 0;
 
 
         //int rnd = Random.Range(1, 10);
@@ -20,15 +27,40 @@ public class GameDirector : MonoBehaviour
 
         //textUI.text = "Game Start";
 
-
+        nowNum = ReturnRandomNum();
+        nextNum = ReturnRandomNum();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        nowNumImage.sprite = numImage[ReturnNumImageIndex(nowNum)];
+        nextNumImage.sprite = numImage[ReturnNumImageIndex(nextNum)];
+        leftNumImage.sprite = numImage[ReturnNumImageIndex(leftNum)];
+        rightNumImage.sprite = numImage[ReturnNumImageIndex(rightNum)];
     }
+
+    private int ReturnNumImageIndex(int num)
+    {
+        return num + 9;
+    }
+
+    public int ReturnRandomNum()
+    {
+        int i = 0;
+        while (i == 0 || i == -5 || i == 5)
+        {
+            i = Mathf.CeilToInt(Random.value * 19.0f - 9.0f);
         }
+        return i;
+    }
+
+    public void SwitchNextNum()
+    {
+        nowNum = nextNum;
+        nextNum = ReturnRandomNum();
+    }
+}
 
 
 
---
+++ Assets/Scripts/velt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class velt : MonoBehaviour
+{
+    [SerializeField]
+    GameDirector director;
+
+    public Slider slider;
+    public float time;
+    [SerializeField]
+    public float velocity = 0.25f;
+    float value = 1.00f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = this.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value -= velocity * Time.deltaTime;
+        if(value <= 0.0f)
+        {
+            /* ストックに送る処理 */
+
+            director.SwitchNextNum();
+            value = 1.00f;
+        }
+        slider.value = value;
+    }
+}
